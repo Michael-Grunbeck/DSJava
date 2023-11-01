ace6b23f2695534830266e00f0f39f5a3ac65480
Michael-Grunbeck <24mgrunbeck@derryfield.org>
2023-11-01 10:58:22 -0400

    HW08

---
+++ Unit03/HW08/HalfLife.java
@@ -0,0 +1,18 @@
+/*
+ * Michael Grunbeck
+ * 10/30
+ * Mr. Eng
+ */
+
+public class HalfLife{
+    public static void main(String[] args) throws Exception{
+        double material = 100;
+        int numyears = 0;
+        while (material>=1){
+            material = material/2;
+            numyears=numyears+30;
+
+        }
+        System.out.print("The amount of material left is "+ String.format("%.2f",material)+"% and the time it took for this is "+numyears+" years");
+    }
+}
---
+++ Unit03/HW08/Intrest.java
@@ -0,0 +1,33 @@
+//Michael Grunbeck
+//10/30
+
+import java.util.Scanner;
+
+public class Intrest {
+    public static void main(String[] args) throws Exception{
+        Scanner a = new Scanner (System.in);
+        System.out.print("What is the amount of money you start with? ");
+        double money = a.nextDouble();
+        System.out.print("What is the interest rate? ");
+        double apr = a.nextDouble();
+        System.out.print("How many years would you like to calculate the intrest rate for: ");
+        int years = a.nextInt();
+        int x = 0;
+        double intrestEarned=0;
+        
+
+    System.out.println("|  Year  |   Intrest Earned ($)   |  Total Balance ($)  |");
+    System.out.println("_________________________________________________________");
+
+    while(x<years){
+        x=x+1;
+        intrestEarned=(apr/100)*money;
+        money=money+intrestEarned;
+        System.out.println(String.format("|   %02d   |  %12.2f          |     %10.2f      |",x,intrestEarned,money));
+        
+
+    }
+
+    }
+
+}
