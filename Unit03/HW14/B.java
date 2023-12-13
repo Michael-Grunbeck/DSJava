2315b63fd58b0816ca6bcba681c395ffc509aee5
Michael-Grunbeck <24mgrunbeck@derryfield.org>
2023-12-13 18:39:27 -0500

    hw14

---
+++ Unit03/HW14/B.java
@@ -0,0 +1,10 @@
+public class B{
+    public static void main (String[] args) throws Exception{
+        double [] numbers = {2.2,1.0,3.7,4.1,88.0};
+        
+        for(int i = 0; i<numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+
+    }
+}
